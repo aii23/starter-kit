bd450be94827d8a7092e70264d558c3b7a514ff2
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-01-28 19:19:57 +0300

    added transferTo

--- packages/chain/src/balances.ts
+++ packages/chain/src/balances.ts
@@ -1,36 +1,56 @@
 import {
-  RuntimeModule,
-  runtimeModule,
-  state,
-  runtimeMethod,
-} from "@proto-kit/module";
-import { State, StateMap, assert } from "@proto-kit/protocol";
-import { Field, Provable, PublicKey, Struct, UInt64 } from "o1js";
+    RuntimeModule,
+    runtimeModule,
+    state,
+    runtimeMethod,
+} from '@proto-kit/module';
+import { State, StateMap, assert } from '@proto-kit/protocol';
+import { Field, Provable, PublicKey, Struct, UInt64 } from 'o1js';
 
 interface BalancesConfig {
-  totalSupply: UInt64;
+    totalSupply: UInt64;
 }
 
 @runtimeModule()
 export class Balances extends RuntimeModule<BalancesConfig> {
-  @state() public balances = StateMap.from<PublicKey, UInt64>(
-    PublicKey,
-    UInt64
-  );
-
-  @state() public circulatingSupply = State.from<UInt64>(UInt64);
-
-  @runtimeMethod()
-  public addBalance(address: PublicKey, amount: UInt64): void {
-    const circulatingSupply = this.circulatingSupply.get();
-    const newCirculatingSupply = circulatingSupply.value.add(amount);
-    assert(
-      newCirculatingSupply.lessThanOrEqual(this.config.totalSupply),
-      "Circulating supply would be higher than total supply"
+    @state() public balances = StateMap.from<PublicKey, UInt64>(
+        PublicKey,
+        UInt64
     );
-    this.circulatingSupply.set(newCirculatingSupply);
-    const currentBalance = this.balances.get(address);
-    const newBalance = currentBalance.value.add(amount);
-    this.balances.set(address, newBalance);
-  }
+
+    @state() public circulatingSupply = State.from<UInt64>(UInt64);
+
+    @runtimeMethod()
+    public addBalance(address: PublicKey, amount: UInt64): void {
+        const circulatingSupply = this.circulatingSupply.get();
+        const newCirculatingSupply = circulatingSupply.value.add(amount);
+        assert(
+            newCirculatingSupply.lessThanOrEqual(this.config.totalSupply),
+            'Circulating supply would be higher than total supply'
+        );
+        this.circulatingSupply.set(newCirculatingSupply);
+        const currentBalance = this.balances.get(address);
+        const newBalance = currentBalance.value.add(amount);
+        this.balances.set(address, newBalance);
+
+        console.log(
+            `Account: ${address.toBase58()}. Prev balance: ${currentBalance.value.toString()}. New Balance: ${newBalance.toString()}`
+        );
+    }
+
+    @runtimeMethod()
+    public transferFrom(from: PublicKey, to: PublicKey, amount: UInt64): void {
+        console.log(`Transfer from: ${from.toBase58()} to ${to.toBase58()}`);
+        let currentBalance = this.balances.get(from).orElse(UInt64.from(0));
+        console.log(
+            `From balance: ${this.balances.get(from).value.toString()}`
+        );
+        console.log(`From balance: ${currentBalance.toString()}`);
+        currentBalance.assertGreaterThan(amount);
+
+        let addrCurrentBalance = this.balances.get(to).orElse(UInt64.from(0));
+
+        this.balances.set(from, currentBalance.sub(amount));
+        this.balances.set(to, addrCurrentBalance.add(amount));
+    }
 }
--- packages/chain/test/balances.test.ts
+++ packages/chain/test/balances.test.ts
@@ -1,47 +1,65 @@
-import { TestingAppChain } from "@proto-kit/sdk";
-import { PrivateKey, UInt64 } from "o1js";
-import { Balances } from "../src/balances";
-import { log } from "@proto-kit/common";
+import { TestingAppChain } from '@proto-kit/sdk';
+import { PrivateKey, UInt64 } from 'o1js';
+import { Balances } from '../src/balances';
+import { log } from '@proto-kit/common';
 
-log.setLevel("ERROR");
+log.setLevel('ERROR');
 
-describe("balances", () => {
-  it("should demonstrate how balances work", async () => {
-    const appChain = TestingAppChain.fromRuntime({
-      modules: {
-        Balances,
-      },
-    });
+describe('balances', () => {
+    it('should demonstrate how balances work', async () => {
+        const appChain = TestingAppChain.fromRuntime({
+            modules: {
+                Balances,
+            },
+        });
 
-    appChain.configurePartial({
-      Runtime: {
-        Balances: {
-          totalSupply: UInt64.from(10000),
-        },
-      },
-    });
+        appChain.configurePartial({
+            Runtime: {
+                Balances: {
+                    totalSupply: UInt64.from(10000),
+                },
+            },
+        });
 
-    await appChain.start();
+        await appChain.start();
 
-    const alicePrivateKey = PrivateKey.random();
-    const alice = alicePrivateKey.toPublicKey();
+        const alicePrivateKey = PrivateKey.random();
+        const alice = alicePrivateKey.toPublicKey();
 
-    appChain.setSigner(alicePrivateKey);
+        const bobPrivateKey = PrivateKey.random();
+        const bob = bobPrivateKey.toPublicKey();
 
-    const balances = appChain.runtime.resolve("Balances");
+        appChain.setSigner(alicePrivateKey);
 
-    const tx1 = await appChain.transaction(alice, () => {
-      balances.addBalance(alice, UInt64.from(1000));
-    });
+        const balances = appChain.runtime.resolve('Balances');
 
-    await tx1.sign();
-    await tx1.send();
+        const tx1 = await appChain.transaction(alice, () => {
+            balances.addBalance(alice, UInt64.from(1000));
+        });
 
-    const block = await appChain.produceBlock();
+        await tx1.sign();
+        await tx1.send();
 
-    const balance = await appChain.query.runtime.Balances.balances.get(alice);
+        const tx2 = await appChain.transaction(alice, () => {
+            balances.addBalance(alice, UInt64.from(1000));
+        });
 
-    expect(block?.transactions[0].status.toBoolean()).toBe(true);
-    expect(balance?.toBigInt()).toBe(1000n);
-  }, 1_000_000);
+        await tx2.sign();
+        await tx2.send();
+
+        const tx3 = await appChain.transaction(alice, () => {
+            balances.transferFrom(alice, bob, UInt64.from(1000));
+        });
+
+        await tx3.sign();
+        await tx3.send();
+
+        const block = await appChain.produceBlock();
+
+        const balance =
+            await appChain.query.runtime.Balances.balances.get(alice);
+
+        expect(block?.transactions[0].status.toBoolean()).toBe(true);
+        expect(balance?.toBigInt()).toBe(1000n);
+    }, 1_000_000);
 });
